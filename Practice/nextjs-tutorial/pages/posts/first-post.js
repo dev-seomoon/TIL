32d275cb6864b871b645f7e3c2646b889442e361
dev-seomoon <seomoon.frontend@gmail.com>
2022-06-29 15:42:53 +0900

    nextjs-blog: css

---
+++ Practice/nextjs-tutorial/pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+    </Layout>
+  );
+}
