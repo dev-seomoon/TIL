3786f1ae815c3088efd1b7fc53a315632466f8fd
dev-seomoon <seomoon.frontend@gmail.com>
2022-07-29 14:22:17 +0900

    TIL

---
+++ Algorithm/문제/boj2512.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check() {
+
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int N, M;
+  cin >> N;
+  int req[N];
+  for (int i = 0; i < N; i++) {
+    cin >> req[i];
+  }
+  cin >> M;
+
+
+}
+
+// cin >> n;
+// cout << result;
+
+// req는 정렬할 필요 없음
+
+/*
+K가 특정 범위를 넘어가면 답이 안되기 때문에 사실상 정렬된 것이나 마찬가지라서
+이분탐색이 가능한 것 
+*/
+
+
+// K 최솟값 : M / N, K 최댓값 : req 최대값
+
+/*
+
+
+*/
